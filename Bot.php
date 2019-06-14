40def71ee7bdd094b4885edbe2f997d1bd0df578
ArsenOOO7 <30925793+ArsenOOO7@users.noreply.github.com>
2019-06-14 12:52:49 +0300

    Add files via upload

---
+++ Bot.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @author ArsenOOO7
+ *
+ */
+
+require_once(__DIR__."/VkGroupBot.php");
+
+class Bot{
+	
+	/** @var VkGroupBot */
+	private $api;
+	
+	
+	
+	/**
+	* @param VkGroupBot $api
+	*
+	*/
+	function __construct(VkGroupBot $api){
+		
+		$this->api = $api;
+		
+	}
+	
+	
+	
+	/**
+	* @param Array $event
+	*
+	*/
+	function exeMessage($event){
+		
+		$peerId = $event["peer_id"];
+		$message = $event["text"];
+		$messageID = $event["id"];
+		
+		
+		$messages = $this->api->getConfig()["messages"];
+		
+		$answer = "Чаго?";
+		
+		foreach($messages as $msg => $resp){
+			
+			if($msg == $message){
+				
+				$answer = $resp;
+				break;
+				
+			}
+		}
+		
+		$this->api->sendMessage($peerId, $answer, $messageID);
+	}
+}
+
+
+if(!file_exists(__DIR__."/Config.json")){
+	
+	$toJson = [
+	
+		"groupToken" => "",
+		"usrToken" => "",
+		"group_id" => 0,
+		"reply" => false,
+		"messages" => [
+		
+			"привет!" => "Привет!",
+			"как дела?" => "Да норм, а у тебя?",
+			"чо делаешь?" => "Работаю в отличии от тебя"
+			
+		]
+	];
+	
+	file_put_contents(__DIR__."/Config.json", json_encode($toJson, JSON_PRETTY_PRINT | JSON_BIGINT_AS_STRING | JSON_UNESCAPED_UNICODE));
+	die("!!! Заполните конфиг !!!");
+	
+}
+
+$config = json_decode(file_get_contents(__DIR__."/Config.json"), true);
+$vkBot = new VkGroupBot($config);
---
+++ VkGroupBot.php
@@ -0,0 +1,189 @@
+<?php
+
+/**
+ * @author ArsenOOO7
+ * Simple LongPoll Bot for Vk
+*/
+
+require_once(__DIR__."/Bot.php");
+
+class VkGroupBot{
+	
+	const API = "https://api.vk.com/method/";
+	const VERSION = "5.80";
+	
+	
+	/** @var Array */
+	private $longPoll = [];
+	/** @var Array */
+	private $config = [];
+	
+	/** @var Bot */
+	private $bot;
+	
+	
+	
+	
+	/**
+	* @param Array $config
+	* @param Boolean $loop
+	*
+	*/
+	function __construct($config = [], $loop = true){
+		
+		$this->config = $config;
+		$this->bot = new Bot($this);
+		
+		$this->getLongPoll();
+		
+		printf("!!! Начинаю работу !!!".PHP_EOL);
+		printf(" --- Работаю... --- ".PHP_EOL);
+		
+		if($loop)
+			$this->startWorking();
+		
+	}
+	
+	
+	
+	function getLongPoll(){
+		
+		$args = [];
+		
+		$args["access_token"] = $this->config["usrToken"];
+		$args["group_id"] = $this->config["group_id"];
+		$args["v"] = VkGroupBot::VERSION;
+		
+		
+		$this->longPoll = json_decode(file_get_contents(VkGroupBot::API . "groups.getLongPollServer?".http_build_query($args)), true)["response"];
+		
+	}
+	
+	
+	
+	/**
+	* @param String $method
+	* @param Array $args
+	*
+	*/
+	function sendRequest($method, $args = []){
+		
+		$args["access_token"] = $this->config["groupToken"];
+		$args["v"] = VkGroupBot::VERSION;
+		
+		return json_decode(file_get_contents(VkGroupBot::API . $method."?".http_build_query($args)), true);
+		
+	}
+	
+	
+	
+	/**
+	* @param Integer $userId
+	* @return String
+	*
+	*/
+	function getUserName($userId){
+		
+		$request = $this->sendRequest("users.get", ["user_ids" => $userId]);
+		
+		return $request[0]["first_name"]." ".$request[0]["last_name"];
+		
+	}
+	
+	
+	
+	/**
+	* @param Integer $peer_id
+	* @param String $message
+	* @param Integer $repliedId
+	*
+	*/
+	function sendMessage($peer_id, $message, $repliedId = null){
+		
+		$args["peer_id"] = $peer_id;
+		$args["message"] = $message;
+		
+		if(isset($repliedId) and $this->config["reply"])
+			$args["forward_messages"] = $repliedId;
+		
+		$this->sendRequest("messages.send", $args);
+		
+	}
+	
+	
+	
+	/**
+	* @return Bot
+	*
+	*/
+	function getBot(){
+		
+		return $this->bot;
+		
+	}
+	
+	
+	
+	/**
+	* @return Config | Array
+	*
+	*/
+	function getConfig(){
+		
+		return $this->config;
+		
+	}
+	
+	
+	
+	function startWorking(){
+		
+		$ts = $this->longPoll["ts"];
+		
+		while(true){
+			
+			$server = $this->longPoll["server"];
+			$key = $this->longPoll["key"];
+			
+			$response = json_decode(file_get_contents($server."?act=a_check&key=".$key."&ts=".$ts."&wait=5"), true);
+			
+			if(isset($response["failed"])){
+				
+				switch($response["failed"]){
+					
+					case 1:
+					
+						$ts = $response["ts"];
+						
+					break;
+					
+					case 2:
+					case 3:
+						
+						$this->getLongPoll();
+						
+					break;
+					
+				}
+				
+				continue;
+				
+			}
+			
+			$ts = $response["ts"];
+			
+			foreach($response["updates"] as $update){
+				
+				switch(array_shift($update)){
+					
+					case "message_new":
+						
+						$this->bot->exeMessage($update["object"]);
+						
+					break;
+					
+				}
+			}
+		}
+	}
+}
